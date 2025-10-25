2138749b1e1c17678adf79376d3db55ec9bf49d2
Neha Gupta <nehaattrash@gmail.com>
2025-10-25 16:50:41 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
- 
+import React from "react";
+import ReactDOM from "react-dom/client";
    const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React");
 
     /** Nested div
@@ -26,8 +27,8 @@
     */
 
    const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]),
-    React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")])
+    React.createElement("div",{id:"child1"},[React.createElement("h1",{id:"h11"},"I'm h1 tag"),React.createElement("h2",{id:"h12"},"I'm h2 tag")]),
+    React.createElement("div",{id:"child2"},[React.createElement("h1",{id:"h21"},"I'm h1 tag"),React.createElement("h2",{id:"h22"},"I'm h2 tag")])
 ]);
    const root = ReactDOM.createRoot(document.getElementById("root"));
    root.render(parent);
